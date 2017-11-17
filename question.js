34ee6dfdf18393e3a377629d351a4e3191fc546b
Sara Smith <sarasmith887@gmail.com>
2017-11-17 22:18:27 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,11 +1,22 @@
-'use strict';
-
-
 class Question {
 constructon(content){
 this.content= content
+this.save()
 
 }
+
+save(){
 this.constructor._All.push(this)
+}
+
+static All() {
+return this._All;
+}
+
+static Find(id) {
+return this.All()[id-1]
+}
 
 }
+
+Question._All = []
